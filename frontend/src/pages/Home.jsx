540b0f4f0b375925ec6773f183e1d759ba4935b5
Sakshi <sakshimundhe2008@gmail.com>
2026-06-28 16:39:47 +0530

    Initial frontend setup

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import "../styles/Home.css";
+function Home() {
+  return (
+    <div className="home">
+      <h1>TalentMind AI</h1>
+      <p>Beyond Keywords. Hire Smarter with AI.</p>
+
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default Home;
